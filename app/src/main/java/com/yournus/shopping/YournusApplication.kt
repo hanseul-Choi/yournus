7e02abcdd611f36ec214ce48fc86bccb380641db
hanseuk-Choi <hschoi5542@gmail.com>
2025-06-21 19:42:26 +0900

    fix : Application 정의부 수정

---
+++ app/src/main/java/com/yournus/shopping/YournusApplication.kt
@@ -0,0 +1,8 @@
+package com.yournus.shopping
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class YournusApplication : Application() {
+}
--- app/src/main/java/com/yournus/shopping/ui/YournusApplication.kt
+++
@@ -1,4 +0,0 @@
-package com.yournus.shopping.ui
-
-class YournusApplication {
-}
